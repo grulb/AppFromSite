fc14c306b1692245366c607534f546897a7c7adc
GreeiLLL <103733640+grulb@users.noreply.github.com>
2025-02-13 12:05:11 +0300

    Initial commit

---
+++ app/src/main/java/com/example/appfromsite/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.appfromsite
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.appfromsite.databinding.ActivityAuthorizationBinding
+import com.example.appfromsite.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, 0)
+            insets
+        }
+
+        binding.authorizeButton.setOnClickListener {
+            goAuthorize()
+        }
+    }
+
+    private fun goAuthorize() {
+        startActivity(Intent(this, AuthorizationActivity::class.java))
+    }
+}
